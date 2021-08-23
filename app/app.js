b9d61dabdb4f9f0e8d7a7904c4d49989eb975ce1
ans9611 <ans9611@gmail.com>
2021-08-22 21:24:39 -0400

    updated front-end

--- app/app.js
+++ app/app.js
@@ -1,135 +1,133 @@
-const addEvents = require("./auth/events")
-const store = require("./store")
+const addEvents = require('./auth/events')
+const store = require('./store')
 
 $(() => {
-  $("#sign-up").on("submit", addEvents.onSignUp);
-  $("#sign-in").on("submit", addEvents.onSignIn);
-  $("#sign-out").on("submit", addEvents.onSignOut);
-  $("#new-portfolio").on("submit", addEvents.onNewPortfolio);
-  $("#show-data").on("submit", addEvents.onShowData);
-  $("#change-password").on("submit", addEvents.onChangePassword)
+  $('#sign-up').on('submit', addEvents.onSignUp)
+  $('#sign-in').on('submit', addEvents.onSignIn)
+  $('#sign-out').on('submit', addEvents.onSignOut)
+  $('#new-portfolio').on('submit', addEvents.onNewPortfolio)
+  $('#show-data').on('submit', addEvents.onShowData)
+  $('#change-password').on('submit', addEvents.onChangePassword)
 
-  $("#account-info").on("click", ".delete-member", addEvents.onDeleteData);
-  $("#account-info").on("submit", ".update-member", addEvents.onUpdateData);
+  $('#account-info').on('click', '.delete-member', addEvents.onDeleteData)
+  $('#account-info').on('submit', '.update-member', addEvents.onUpdateData)
 
+  $('#sign-out').hide()
+  $('#new-portfolio').hide()
+  $('#show-data').hide()
+  $('#change-password').hide()
+  $(".portfolio-page").hide();
 
-  $("#sign-out").hide()
-  $("#new-portfolio").hide();
-  $("#show-data").hide()
+  $('.navbar_tooleBtn').on('click', () => {
+    $('.navbar_menu').toggle('active')
+    $('.navbar_icons').toggle('active')
+  })
 
+  $('.sign-message a').on('click', () => {
+    $('form').animate({ height: 'toggle', opacity: 'toggle' }, 'slow')
+  })
+  $('.update-message').on('click', () => {
+    $('#change-password').show()
+  })
+})
 
-
-
-   $(".navbar_tooleBtn").on("click", () => {
-     $(".navbar_menu").toggle("active");
-     $(".navbar_icons").toggle("active");
-   });
-
-  $(".sign-message a").on("click", () => {
-    $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
-  });
-
-});
-
-
-
-let totalBalance = 0;
-let cash = 0;
-let bonds = 0;
-let stocks = 0;
+let totalBalance = 0
+let cash = 0
+let bonds = 0
+let stocks = 0
 let ai = 0
 setTimeout(function () {
   console.log(store)
-  console.log(store.totalBalance);
+  console.log(store.totalBalance)
   totalBalance = store.totalBalance
-  cash = (store.totalBalance * 0.1).toFixed(2);
-  bonds = (store.totalBalance * 0.4).toFixed(2);
-  stocks = (store.totalBalance * 0.3).toFixed(2);
-  ai = (store.totalBalance * 0.2).toFixed(2);
-  drawChart();
-  drawChart2();
-  drawChart3();
-  drawChart4();
-}, 9000);
+  cash = (store.totalBalance * 0.1).toFixed(2)
+  bonds = (store.totalBalance * 0.4).toFixed(2)
+  stocks = (store.totalBalance * 0.3).toFixed(2)
+  ai = (store.totalBalance * 0.2).toFixed(2)
+  drawChart()
+  drawChart2()
+  drawChart3()
+  drawChart4()
+}, 9000)
 
- google.charts.load("current", { packages: ["corechart"] });
- google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-   let data = google.visualization.arrayToDataTable([
-     [`Total Account: ${totalBalance} `, "Dollars"],
-     [`Cash: $${cash}`, 30],
-     [`Bonds: $${bonds}`, 40],
-     [`Stocks: $${stocks}`, 15],
-     [`Alternative Investments: $${ai}`, 15],
-   ]);
+google.charts.load('current', { packages: ['corechart'] })
+google.charts.setOnLoadCallback(drawChart)
+function drawChart () {
+  const data = google.visualization.arrayToDataTable([
+    [`Total Account: ${totalBalance} `, 'Dollars'],
+    [`Cash: $${cash}`, 30],
+    [`Bonds: $${bonds}`, 40],
+    [`Stocks: $${stocks}`, 15],
+    [`Alternative Investments: $${ai}`, 15]
+  ])
 
-   let options = {
-     title: "Portfolio for Conservative Investor",
-     is3D: true,
-   };
+  const options = {
+    title: 'Portfolio for Conservative Investor',
+    is3D: true
+  }
 
-   let chart = new google.visualization.PieChart(
-     document.getElementById("piechart_3d")
-   );
-   chart.draw(data, options);
- }
+  const chart = new google.visualization.PieChart(
+    document.getElementById('piechart_3d')
+  )
+  chart.draw(data, options)
+}
 
- google.charts.setOnLoadCallback(drawChart2);
-function drawChart2() {
-  let data = google.visualization.arrayToDataTable([
-    [`Total Account: ${totalBalance} `, "Dollars"],
+google.charts.setOnLoadCallback(drawChart2)
+function drawChart2 () {
+  const data = google.visualization.arrayToDataTable([
+    [`Total Account: ${totalBalance} `, 'Dollars'],
     [`Cash: $${cash}`, 10],
     [`Bonds: $${bonds}`, 40],
     [`Stocks: $${stocks}`, 30],
-    [`Alternative Investments: $${ai}`, 20],
-  ]);
+    [`Alternative Investments: $${ai}`, 20]
+  ])
 
-  let options = {
-    title: "Portfolio for Moderate Investor",
-    is3D: true,
-  };
+  const options = {
+    title: 'Portfolio for Moderate Investor',
+    is3D: true
+  }
 
-  let chart = new google.visualization.PieChart(
-    document.getElementById("piechart_low")
-  );
-  chart.draw(data, options);
+  const chart = new google.visualization.PieChart(
+    document.getElementById('piechart_low')
+  )
+  chart.draw(data, options)
 }
 
- google.charts.setOnLoadCallback(drawChart3);
- function drawChart3() {
-   let data = google.visualization.arrayToDataTable([
-     [`Total Account: ${totalBalance} `, "Dollars"],
-     [`Cash: $${cash}`, 0],
-     [`Bonds: $${bonds}`, 20],
-     [`Stocks: $${stocks}`, 50],
-     [`Alternative Investments: $${ai}`, 30],
-   ]);
+google.charts.setOnLoadCallback(drawChart3)
+function drawChart3 () {
+  const data = google.visualization.arrayToDataTable([
+    [`Total Account: ${totalBalance} `, 'Dollars'],
+    [`Cash: $${cash}`, 0],
+    [`Bonds: $${bonds}`, 20],
+    [`Stocks: $${stocks}`, 50],
+    [`Alternative Investments: $${ai}`, 30]
+  ])
 
-   let options = {
-     title: "Portfolio for Aggresive Investor",
-     is3D: true,
-   };
-
-   let chart = new google.visualization.PieChart(
-     document.getElementById("piechart_high")
-   );
-   chart.draw(data, options);
- }
-
-  google.charts.setOnLoadCallback(drawChart4);
-  function drawChart4() {
-    let data = google.visualization.arrayToDataTable([
-      [`Lottery Tickets: $${totalBalance} `, "Dollars"],
-      [`Lottery Tickets: $${totalBalance}`, 100],
-    ]);
-
-    let options = {
-      title: "Portfolio for Impulsive Man",
-      is3D: true,
-    };
-
-    let chart = new google.visualization.PieChart(
-      document.getElementById("piechart_crazy")
-    );
-    chart.draw(data, options);
+  const options = {
+    title: 'Portfolio for Aggresive Investor',
+    is3D: true
   }
+
+  const chart = new google.visualization.PieChart(
+    document.getElementById('piechart_high')
+  )
+  chart.draw(data, options)
+}
+
+google.charts.setOnLoadCallback(drawChart4)
+function drawChart4 () {
+  const data = google.visualization.arrayToDataTable([
+    [`Lottery Tickets: $${totalBalance} `, 'Dollars'],
+    [`Lottery Tickets: $${totalBalance}`, 100]
+  ])
+
+  const options = {
+    title: 'Portfolio for Impulsive Man',
+    is3D: true
+  }
+
+  const chart = new google.visualization.PieChart(
+    document.getElementById('piechart_crazy')
+  )
+  chart.draw(data, options)
+}
--- app/auth/ui.js
+++ app/auth/ui.js
@@ -1,10 +1,10 @@
-const store = require("../store");
-const api = require("./api");
-const app = require("../app")
+const store = require('../store')
+const api = require('./api')
+const app = require('../app')
 
 const storeData = function (response) {
-  store.member = response.member;
-  let tableFirst =
+  store.member = response.member
+  const tableFirst =
   `<table class="styled-table">
     <thead>
         <tr>
@@ -14,13 +14,13 @@ const storeData = function (response) {
             <th>Update</th>
             <th>Delete</th>
         </tr>
-    </thead>`;
+    </thead>`
 
-  store.memberHtml = "";
-  let tableLast = `</table>`;
+  store.memberHtml = ''
+  const tableLast = '</table>'
 
-    store.member.forEach((member) => {
-      store.memberHtml += `
+  store.member.forEach((member) => {
+    store.memberHtml += `
     <tbody>
         <tr>
             <td>2016</td>
@@ -33,131 +33,125 @@ const storeData = function (response) {
             <td><button class='delete-member' data-id=${member._id}>Remove</button></td>
       </tr>
     </tbody>
-`;});
+`
+  })
 
-  let table = tableFirst + store.memberHtml + tableLast;
+  const table = tableFirst + store.memberHtml + tableLast
 
-  $("#account-info").html(table);
-};
-
-
+  $('#account-info').html(table)
+}
 
 const onSignUpSuccess = (response) => {
-  $("#message").text(
+  $('#message').text(
     `Thank you for signing Up! ${response.user.email} Sign in To play Game!!`
-  );
-
-};
+  )
+}
 
 const onSighUpFailure = () => {
-  $("#message").text(`Unable to Sign Up...:( Check email or password`);
-  $("#sign-up").trigger("reset");
-};
+  $('#message').text('Unable to Sign Up...:( Check email or password')
+  $('#sign-up').trigger('reset')
+}
 
 const onSignInSuccess = (response) => {
-  $("#message").text(`Welcome Back! ${response.user.email}`);
-  store.token = response.user.token;
-  store.id = response.user._id;
-  $("#sign-up").trigger("reset");
-$("#new-portfolio").show();
-   $("#show-data").show();
-   $("#sign-in").hide()
-   $("#sign-out").show()
-
-};
+  $('#message').text(`Welcome Back! ${response.user.email}`)
+  store.token = response.user.token
+  store.id = response.user._id
+  $('#sign-up').trigger('reset')
+  $('#new-portfolio').show()
+  $('#show-data').show()
+  $('#sign-in').hide()
+  $('#sign-out').show()
+  $('.portfolio-page').show()
+}
 
 const onSignInFailure = () => {
-  $("#message").text(`Unable to sign in :( check email or password again!`);
-
-};
+  $('#message').text('Unable to sign in :( check email or password again!')
+}
 
 const onSignOutSuccess = () => {
-  $("#message").text("Successfully Signed out! Thank you! Come Again!");
-  $("#sign-out").hide();
-  $("#new-portfolio").hide();
-  $("#show-data").hide();
-  $("#sign-in").show()
-  $("#account-info").hide();
-};
+  $('#message').text('Successfully Signed out! Thank you! Come Again!')
+  $('#sign-out').hide()
+  $('#new-portfolio').hide()
+  $('#show-data').hide()
+  $('#sign-in').show()
+  $('#account-info').hide()
+  $('.portfolio-page').hide()
+  $('#change-password').hide()
+}
 const onSignOutFailure = () => {
-  $("#message").text(
-    "Unable to Signed out! :)"
-  );
-};
+  $('#message').text(
+    'Unable to Signed out! :)'
+  )
+}
 
 const onChangePasswordSuccess = () => {
-  $("#message").text("Successfully Changed Password!");
+  $('#message').text('Successfully Changed Password!')
 }
 
 const onChangePasswordFailure = () => {
-  $("#message").text("Not Successfully Changed Password!");
-};
+  $('#message').text('Not Successfully Changed Password!')
+}
 
 const onNewPortfolioSuccess = (response) => {
   console.log(response)
   store.balance = response.member.balance
-  store.risk = response.member.risk;
-  store.name = response.member.name;
+  store.risk = response.member.risk
+  store.name = response.member.name
   console.log(store.name)
   console.log(store)
-   const data = response.member;
-   let totalBalance = 0;
-   for (let i = 0; i < data.length; i++) {
-     let num = Number(data[i].balance);
-     totalBalance += num;
-   }
-$("#new-portfolio").show();
-  $("#message").text("Successfully Get the portfolio");
-};
-
+  const data = response.member
+  let totalBalance = 0
+  for (let i = 0; i < data.length; i++) {
+    const num = Number(data[i].balance)
+    totalBalance += num
+  }
+  $('#new-portfolio').show()
+  $('#message').text('Successfully Get the portfolio')
+  $('.chart').show()
+}
 
 const onNewPortfolioFailure = () => {
-  $("#message").text(`Unable to get Portfolio`);
-};
+  $('#message').text('Unable to get Portfolio')
+}
 
 const onShowDataSuccess = (response) => {
   const data = response.member
   let totalBalance = 0
   for (let i = 0; i < data.length; i++) {
-    let num = Number(data[i].balance);
-    totalBalance += num;
+    const num = Number(data[i].balance)
+    totalBalance += num
   }
   console.log(data)
-  console.log(totalBalance);
+  console.log(totalBalance)
   app.totalBalance = totalBalance
-  $("#message").text("Successfully Get the Data");
+  $('#message').text('Successfully Get the Data')
   storeData(response)
 
-     store.totalBalance = totalBalance;
-     console.log(store.totalBalance);
-     $("#account-info").show()
-};
-
-
-
-
+  store.totalBalance = totalBalance
+  console.log(store.totalBalance)
+  $('#account-info').show()
+}
 
 const onShowDataFailure = () => {
-  $("#message").text("Unable to Get the Data");
-};
-
+  $('#message').text('Unable to Get the Data')
+}
 
 const onDeleteDataSuccess = () => {
-  $("#message").text("Data Deleted!");
-  api.showData().then(storeData);
-};
+  $('#message').text('Data Deleted!')
+  api.showData().then(storeData)
+}
 
 const onDeleteDataFailure = () => {
-  $("#message").text("Unable to delete the Data");
-};
+  $('#message').text('Unable to delete the Data')
+}
 
 const onUpdateDataSuccess = (response) => {
-   $("#message").text("Data Updated!");
-   api.showData().then(storeData);
+  $('#message').text('Data Updated!')
+  api.showData().then(storeData)
 }
 const onUpdateDataFailure = () => {
-  $("#message").text("Unable to Update the Data");
-};
+  $('#message').text('Unable to Update the Data')
+}
 
 module.exports = {
   onSignUpSuccess,
@@ -175,5 +169,5 @@ module.exports = {
   onUpdateDataSuccess,
   onUpdateDataFailure,
   onChangePasswordSuccess,
-  onChangePasswordFailure,
-};
+  onChangePasswordFailure
+}
